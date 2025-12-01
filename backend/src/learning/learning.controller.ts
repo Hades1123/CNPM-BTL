ab40b7ac2eba5e6d6e0ee746b655a3864d81792d
Hades1123 <dp1.1a7.si@gmail.com>
2025-12-01 15:00:45 +0700

    something ?

--- backend/src/learning/learning.controller.ts
+++ backend/src/learning/learning.controller.ts
@@ -1,20 +1,40 @@
-import { Controller, Get, Headers, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import {
+    BadRequestException,
+    Controller,
+    Get,
+    Headers,
+    HttpCode,
+    HttpException,
+    HttpStatus,
+    Param,
+    UseInterceptors,
+} from '@nestjs/common';
 import { LearningService } from './learning.service';
 import { Public } from '@/decorators/customize';
+import { ExcludeNullInterceptor } from '@/common/interceptors/excludeNull.interceptor';
 
 @Controller('learning')
 @Public()
+@UseInterceptors(ExcludeNullInterceptor)
 export class LearningController {
     constructor(private readonly learningService: LearningService) {}
 
     @Get()
     @HttpCode(HttpStatus.OK)
     getText() {
-        return 'This is get method';
+        throw new HttpException('custom message', HttpStatus.BAD_GATEWAY, {
+            cause: new Error(),
+            description: 'error kkk',
+        });
     }
 
     @Get(':id/:cnt')
     getParams_1(@Param('cnt') param: number, @Headers() header: any) {
         return header;
+    }
+
+    @Get('null')
+    getNullResponse() {
+        return null;
     }
 }
